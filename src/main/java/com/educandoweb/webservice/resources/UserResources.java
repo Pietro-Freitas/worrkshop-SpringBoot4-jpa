3812e0b3679dbab655b52d6bbcb946917f651279
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-13 10:37:27 -0300

    User entity and Resource

---
+++ src/main/java/com/educandoweb/webservice/resources/UserResources.java
@@ -0,0 +1,22 @@
+package com.educandoweb.webservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.webservice.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Pietro", "Pietro@gmail.com", "1234", "4321");
+
+        return ResponseEntity.ok().body(u);
+    }
+}
