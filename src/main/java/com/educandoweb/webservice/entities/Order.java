3a42aa72a7bdaad2698be58e89e41eed67efa204
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-13 15:01:32 -0300

    OrderStatus Enum

--- src/main/java/com/educandoweb/webservice/entities/Order.java
+++ src/main/java/com/educandoweb/webservice/entities/Order.java
@@ -3,6 +3,7 @@ package com.educandoweb.webservice.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.educandoweb.webservice.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -24,6 +25,8 @@ public class Order implements Serializable{
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -31,10 +34,11 @@ public class Order implements Serializable{
     public Order() {
     }
 
-    public Order(Integer id, Instant moment, User client) {
+    public Order(Integer id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        if(orderStatus != null) this.orderStatus = orderStatus.getCode();
     }
 
     // Getters and Setters
@@ -60,6 +64,14 @@ public class Order implements Serializable{
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) this.orderStatus = orderStatus.getCode();
     }
 
     //HashCode and Equals
@@ -92,4 +104,5 @@ public class Order implements Serializable{
     public String toString() {
         return "Order [id=" + id + ", moment=" + moment + ", client=" + client + "]";
     }
+
 }
