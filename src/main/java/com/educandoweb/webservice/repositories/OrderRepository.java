06c2068fc7f3cb93973cbabf0fb5b0203e7742b8
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-13 14:41:16 -0300

    Order, instant, ISO 8601 - part 2

---
+++ src/main/java/com/educandoweb/webservice/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.educandoweb.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.educandoweb.webservice.entities.Order;
+
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+    
+}
---
+++ src/main/java/com/educandoweb/webservice/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.webservice.entities.Order;
+import com.educandoweb.webservice.repositories.OrderRepository;
+
+
+@Service
+public class OrderService {
+    
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+
+    
+    public Order findById(Long id){
+        Optional<Order> obj =  orderRepository.findById(id);
+        return obj.get();
+    }
+}
