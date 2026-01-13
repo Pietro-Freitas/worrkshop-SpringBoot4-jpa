5f970a596d63433b794a137f55a6e5bc9f676835
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-13 15:47:51 -0300

    Category

--- src/main/java/com/educandoweb/webservice/config/TestConfig.java
+++ src/main/java/com/educandoweb/webservice/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.webservice.entities.Category;
 import com.educandoweb.webservice.entities.Order;
 import com.educandoweb.webservice.entities.User;
 import com.educandoweb.webservice.entities.enums.OrderStatus;
+import com.educandoweb.webservice.repositories.CategoryRepository;
 import com.educandoweb.webservice.repositories.OrderRepository;
 import com.educandoweb.webservice.repositories.UserRepository;
 
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -33,8 +38,13 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         // Salva no banco de dados
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 }
