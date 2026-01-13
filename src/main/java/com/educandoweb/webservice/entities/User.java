44241b84588b389ed183d225cff1eb7125ac549a
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-13 12:17:24 -0300

    Order, instant, ISO 8601 - part 1

--- src/main/java/com/educandoweb/webservice/entities/User.java
+++ src/main/java/com/educandoweb/webservice/entities/User.java
@@ -1,11 +1,14 @@
 package com.educandoweb.webservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +22,9 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
     
     public User() {
     }
@@ -32,6 +38,10 @@ public class User implements Serializable{
     }
 
     // getters and setters
+    public List<Order> getOrders(){
+        return orders;
+    }
+
     public Long getId() {
         return id;
     }
