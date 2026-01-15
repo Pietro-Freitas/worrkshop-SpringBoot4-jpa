aaf5218ef7bcedcb42f6a07eca5b779126a4a6de
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-15 12:05:32 -0300

    User insert

--- src/main/java/com/educandoweb/webservice/services/UserService.java
+++ src/main/java/com/educandoweb/webservice/services/UserService.java
@@ -20,9 +20,12 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    
     public User findById(Long id){
         Optional<User> obj =  userRepository.findById(id);
         return obj.get();
+    }
+
+    public User insert(User obj){
+        return userRepository.save(obj);
     }
 }
